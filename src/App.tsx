6bfeb4e37b6477c008f3675f422b9f7dabedfebc
halilhasar <h_hasar@hotmail.com>
2025-05-31 12:47:58 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import EmployeeList from './pages/EmployeeList';
+import EmployeeDetail from './pages/EmployeeDetail';
+import EmployeeForm from './pages/EmployeeForm';
+import WorklogForm from './pages/WorklogForm';
+import WorklogTypeList from './pages/WorklogTypeList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="content-container">
+          <Sidebar />
+          <main className="main-content">
+            <Container fluid className="py-4">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/employees" element={<EmployeeList />} />
+                <Route path="/employees/:id" element={<EmployeeDetail />} />
+                <Route path="/employees/new" element={<EmployeeForm />} />
+                <Route path="/employees/edit/:id" element={<EmployeeForm />} />
+                <Route path="/worklogs/new" element={<WorklogForm />} />
+                <Route path="/worklogs/edit/:id" element={<WorklogForm />} />
+                <Route path="/worklog-types" element={<WorklogTypeList />} />
+              </Routes>
+            </Container>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
